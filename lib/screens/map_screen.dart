d9a240f82b14986a98bb764f173b96e6a9474674
loon78 <24004547@siswa.um.edu.my>
2025-03-24 20:55:49 +0800

    Added border to UM in map
    complaints must now be within UM.

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -47,6 +47,22 @@ class _MapScreenState extends State<MapScreen>
 
   final LocationService _locationService = LocationService();
 
+  // Define the border of Universiti Malaya
+  final List<LatLng> _umBorder = [
+    LatLng(3.11928, 101.64288),
+    LatLng(3.11658, 101.64910),
+    LatLng(3.10988, 101.65253),
+    LatLng(3.11683, 101.66395),
+    LatLng(3.13501, 101.66080),
+    LatLng(3.13461, 101.65414),
+    LatLng(3.13083, 101.64849),
+    LatLng(3.12485, 101.64731),
+    
+    // Add more coordinates to define the border
+  ];
+
+  Set<Polygon> _polygons = {};
+
   @override
   void initState() {
     super.initState();
@@ -64,6 +80,7 @@ class _MapScreenState extends State<MapScreen>
     _loadMarkers();
     _initializeLocationServices();
     _initializeAnimations();
+    _loadPolygons();
   }
 
   void _initializeLocationServices() {
@@ -115,6 +132,21 @@ class _MapScreenState extends State<MapScreen>
     } catch (e) {
       debugPrint('Error loading markers: $e');
     }
+  }
+
+  // Load the polygon for UM border
+  Future<void> _loadPolygons() async {
+    setState(() {
+      _polygons = {
+        Polygon(
+          polygonId: PolygonId('um_border'),
+          points: _umBorder,
+          strokeWidth: 2,
+          strokeColor: Colors.blue,
+          fillColor: Colors.blue.withOpacity(0),
+        ),
+      };
+    });
   }
 
   Future<void> _getCurrentLocation() async {
@@ -169,7 +201,6 @@ class _MapScreenState extends State<MapScreen>
       
       // Then get current location
       // await _getCurrentLocation();
-      
       // Show success message
       if (mounted) {
         ScaffoldMessenger.of(context).showSnackBar(
@@ -277,7 +308,26 @@ class _MapScreenState extends State<MapScreen>
     });
   }
 
+  bool _isWithinUM(LatLng point) {
+    // Implement point-in-polygon algorithm
+    bool isInside = false;
+    for (int i = 0, j = _umBorder.length - 1; i < _umBorder.length; j = i++) {
+      if (((_umBorder[i].latitude > point.latitude) != (_umBorder[j].latitude > point.latitude)) &&
+          (point.longitude < (_umBorder[j].longitude - _umBorder[i].longitude) * (point.latitude - _umBorder[i].latitude) / (_umBorder[j].latitude - _umBorder[i].latitude) + _umBorder[i].longitude)) {
+        isInside = !isInside;
+      }
+    }
+    return isInside;
+  }
+
   void _useSelectedLocation() {
+    if (!_isWithinUM(_selectedLocation)) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Please select a location within Universiti Malaya')),
+      );
+      return;
+    }
+
     setState(() {
       _isSelectionMode = false;
       _showDragHint = false;
@@ -328,6 +378,7 @@ class _MapScreenState extends State<MapScreen>
       onMapCreated: _configureMap,
       markers: _isSelectionMode ? {} : _markers,
       circles: _circles,
+      polygons: _polygons,
       myLocationEnabled: _showDefaultLocationMarker,
       zoomControlsEnabled: MapService.defaultMapOptions.zoomControlsEnabled,
       mapToolbarEnabled: MapService.defaultMapOptions.mapToolbarEnabled,
